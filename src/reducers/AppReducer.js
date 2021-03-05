eb4df0a1cb3c71fba72545733622ce7a4e192beb
Andrew Sohrabi <andrewsohrabi@Andrews-MacBook-Pro.local>
2021-03-04 23:53:40 -0800

    edit phoneNumber and Password now work

--- src/reducers/AppReducer.js
+++ src/reducers/AppReducer.js
@@ -1,3 +1,7 @@
+import {ADD_PLANT,DELETE_PLANT, EDIT_PLANT, GET_USER_INFO, GET_PLANT_INFO } 
+from '../actions/appActions'
+
+
 const initialState = {
     users: [ {
         username: "",
@@ -22,23 +26,31 @@ const initialState = {
   export const AppReducer = (state = initialState, action) => {
     console.log("STATE", state);
     switch (action.type) {
-      case "ADD_PLANT":
+      case ADD_PLANT:
         console.log("Action Payload", action.payload);
         return {
           ...state.app,
           plants: [...state.app.plants, action.payload],
         };
-      case "DELETE_PLANT":
+      case DELETE_PLANT:
         return {
           ...state.app,
           plants: state.app.plants.filter((plantObject) => {
             return plantObject.id !== action.payload.id;
           }),
         };
-      case "EDIT_PLANT":
+      case EDIT_PLANT:
         return {
-          user: { ...state.app, plants: [...state.app.plants, action.payload] },
+          users: { ...state.app, plants: [...state.app.plants, action.payload] },
         };
+      case GET_USER_INFO: 
+        return {
+          ...state.auth
+        }
+      case GET_PLANT_INFO:
+        return {
+          plants: {...state.app.plants}
+        }
       default:
         return state;
     }
